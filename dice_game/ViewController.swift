6a9d58ba43a2418ee9479fcaeda35dd8baa5be21
AbuHejleh <abood.kamal.abuhejleh>
2022-09-26 18:42:59 +0300

    Initial Commit

---
+++ dice_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice_game
+//
+//  Created by Abood Abu hejleh on 26/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
